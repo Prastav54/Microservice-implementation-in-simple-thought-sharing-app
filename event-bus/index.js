d53356df4f75634965c01616bebaa03c70c14304
prastav54 <pokharelprastav@gmail.com>
2022-11-29 11:59:01 +0545

    Following feature added
    - Commnent approval / reject service added
    - Event sync concept implemented

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
+const events = [];
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    res.send({status: 'OK'})
-})
+  events.push(event);
 
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log('Listening on 4005')
-})
+  console.log("Listening on 4005");
+});
